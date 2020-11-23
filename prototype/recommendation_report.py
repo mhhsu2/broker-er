81e9b5161912e4729030e66b6cedc2c755b1cddc
skang75 <skang75@illinois.edu>
2020-11-23 11:44:39 +0900

    predict next week's price

--- prototype/recommendation_report.py
+++ prototype/recommendation_report.py
@@ -19,7 +19,7 @@ def predict(df):
     dataset = data.values
     #dataset = data.values
 
-    training_data_len = math.ceil(len(dataset) * 1)
+    training_data_len = math.ceil(len(dataset) * 0.8)
 
     print(training_data_len)
 
@@ -60,70 +60,46 @@ def predict(df):
     #train the model
     model.fit(x_train,y_train, batch_size= 1, epochs=1)
 
+    test_data = scaled_data[training_data_len-30: , :]
+
+
+    #create the data sets x_test and y_test
+    x_test =[]
+    y_test = dataset[training_data_len:, :]
+    for i in range(30, len(test_data)):
+        x_test.append(test_data[i-30:i, 0])
+
+    x_test = np.array(x_test)
+
+    x_test = np.reshape(x_test,(x_test.shape[0], x_test.shape[1], 1))
 
 
 
-    test_data = scaled_data[training_data_len-30: ,:]
+    #get the models predicted price values
 
-    x_test = np.array(test_data)
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
 
-    x_test = x_test[np.newaxis, ...]
-
-    predictionList =[]
-    for i in range(7):
-
-        x_test = np.array(x_test)
-        x_test = np.reshape(x_test,(x_test.shape[0], x_test.shape[1], 1))
-
-        predictions = model.predict(x_test[-30:])
-
-
-        predictions_ = np.squeeze(predictions)
-        predictionList.append(predictions_)
-
-        x_test_ = np.squeeze(x_test)
-
-        x_test=np.append(x_test_,predictions_)
-        x_test = np.reshape(x_test,(1, x_test.shape[0], 1))
-
-
-    predictionList = np.array(predictionList)
-    predictionList = predictionList.reshape(predictionList.shape[0],1)
-    predictionList = scaler.inverse_transform(predictionList)
-    predictionList = np.squeeze(predictionList)
-
-
-    print(predictionList)
-
+    rmse = np.sqrt(np.mean(predictions - y_test)**2)
 
     train = data[:training_data_len]
     valid = data[training_data_len:]
+    print(predictions)
+    print(predictions.shape)
     #numpy array
 
-    #print(valid)
-    #valid['Predictions'] = predictions
-    #print(valid)
+    print(valid)
+    valid['Predictions'] = predictions
+    print(valid)
 
     plt.figure(figsize = (16,8))
-
-    graph = list(np.squeeze(data.values))
-
-    graph.append(predictionList[0])
-
-    plt.plot(graph)
-
-    plt.plot(np.arange(7)+training_data_len ,predictionList)
-    plt.legend(['Train','Predictions'], loc = 'lower right')
+    plt.plot(train['Close'])
+    plt.plot(valid[['Close','Predictions']])
+    plt.legend(['Train','Val', 'Predictions'], loc = 'lower right')
     plt.show()
-
-
-
 
     quote = df
     new_df = quote.filter(['Close'])
-
-
-
     last_30_days = new_df[-30:].values
     last_30_days_scaled = scaler.transform(last_30_days)
 
@@ -134,13 +110,7 @@ def predict(df):
 
     pred_price = model.predict(X_test)
     pred_price = scaler.inverse_transform(pred_price)
-
-
     print(pred_price)
-
-
-
-
 
 
 if __name__ == "__main__":
